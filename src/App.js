28efaff8c3a97c99201fa022a8f1a471339af805
Pedro <1pedrohenri@gmail.com>
2022-04-28 17:30:57 -0300

    initial-commitAndCard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className = "min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
+  <Card/>
+</div>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,46 @@
+import React from "react"
+import profile from "../images/memoji-me.png"
+import { FaGithub, FaLinkedin, FaWhatsapp, FaEnvelope } from 'react-icons/fa';
+
+
+function Card() {
+    return (
+        <div className="w-full">
+            <div className='flex flex-col justify-center max-w-xs mx-auto bg-white shadow-2xl rounded-xl p-5'>
+                <div className="">
+                    <img className="w-32 mx-auto shadow-xl rounded-full drop-shadow-sm" src={profile} alt="profile face" />
+
+                </div>
+                <div className="text-center mt-5">
+                    <p className='text-xl sm:text-2xl font-semibold text-gray-900'>
+                        Pedro Henrique
+                    </p>
+                    <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block border-b-2'>
+                        FullStack Developer
+
+                    </p>
+                    <div className="flex align-center justify-center mt-4">
+                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/pedrinho81" target="_blank" rel="noreferrer">
+                            <FaGithub />
+                            <span className="sr-only">Github</span>
+                        </a>
+                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-800 hover:bg-blue-600 rounded-full hover:text-white transition-colors duration-300" href="https://www.linkedin.com/in/pedro-henrique81/" target="_blank" rel="noreferrer">
+                            <FaLinkedin />
+                            <span className="sr-only">Linkedin</span>
+                        </a>
+                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-green-800 hover:bg-green-800 rounded-full hover:text-white transition-colors duration-300" href="https://api.whatsapp.com/send?phone=5581987227660&text=Olá%2C%20Pedro%20Henrique!" target="_blank" rel="noreferrer">
+                            <FaWhatsapp />
+                            <span className="sr-only">WhatsApp</span>
+                        </a>
+                        <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-black-500 hover:bg-red-900 rounded-full hover:text-white transition-colors duration-300" href="mailto:1pedrohenri@gmail.com" mailTo target="_blank" rel="noreferrer">
+                            <FaEnvelope />
+                            <span className="sr-only">E-mail</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
